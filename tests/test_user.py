f27caea953a552e2944702ae83dd87effc2a0d7d
temugem12 <temuge.max12@gmail.com>
2024-06-15 21:21:54 -0600

    chagned alembic folder name

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,8 +1,32 @@
-from app.main import app
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.config import settings
+from sqlalchemy.ext.declarative import declarative_base
+from app.database import get_db, Base
+#mock dependancy 
+from app import main
 from fastapi.testclient import TestClient
+from app.schemas import UserOut
 
 
-client = TestClient(app)
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@64.23.128.76:{settings.database_port}/{settings.database_name}"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+main.app.dependency_overrides[get_db] = override_get_db
+   
+
+client = TestClient(main.app)
 
 
 def test_root():
@@ -11,7 +35,8 @@ def test_root():
     assert res.status_code == 200 
 
 def test_create_user():
-    res = client.post("/users/", json = {"email": "testing123@gmail.com", "password": "password123"})
-    print (res.json())
-
+    res = client.post("/users/", json = {"email": "tee@gmail.com", "password": "password123"})
+    
+    new_user= UserOut(**res.json())
+    assert new_user.email == "tee@gmail.com"
     assert res.status_code == 201
