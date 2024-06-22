5572c49f894f00b514fe0bbd0eca2f78b66f6fd5
temugem12 <temuge.max12@gmail.com>
2024-06-22 13:54:09 -0600

    turned of the engine in the main file

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from .database import engine, Base
 
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
