8d544d69a13bc3f87b919407c60ec0ded6df8e85
temugem12 <temuge.max12@gmail.com>
2024-06-22 20:42:56 -0600

    push to ubuntu

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Succesfully Deployed using heroku"}
+    return {"message": "Succesfully Deployed using ubuntu"}
