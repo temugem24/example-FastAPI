9b6e2c1e1315c60349b5694f48a72e44c2b30c3c
temugem12 <temuge.max12@gmail.com>
2024-06-23 00:30:55 -0600

    changed home page

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Succesfully Deployed using ubuntu"}
+    return {"message": "Succesfully Deployed using heroku"}
