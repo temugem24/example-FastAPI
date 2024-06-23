3bd56d925144c71f2e9bdaf4e38008e3a6dc6b62
temugem12 <temuge.max12@gmail.com>
2024-06-23 01:16:11 -0600

    turning both base on

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,8 +15,8 @@ TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engin
 
 @pytest.fixture
 def session():
-    # Base.metadata.drop_all(bind=engine)
-    # Base.metadata.create_all(bind=engine)
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
     db = TestingSessionLocal()
     try:
         yield db
