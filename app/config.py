6c264367ef5c38e85a6e3f864f9c18825c350c57
temugem12 <temuge.max12@gmail.com>
2024-06-23 02:21:15 -0600

    commented a bunch of shit

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     database_hostname: str
-    database_name: str
+    # database_name: str
     database_password: str
     database_port: str
     database_username: str
